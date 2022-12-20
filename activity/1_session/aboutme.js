800f3a81ad495eac3f9c99424f268277e13a83d3
FranJimenezQ <flakundo@gmail.com>
2022-12-20 17:13:02 -0600

    Primeros cambios para el ejercicio 1 y 2

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,8 @@
-console.log("hello world");
+var myFullName = "Francisco Jimenez Quesada"
+var myProfession = "FrontEnd Developer"
+var myFuturePosition = "Full Stack Developer"
 
+var frase = "Hola, me llamo " + myFullName + "y me dedico a " + myProfession + "."+
+" Estoy cursando este Máster porque me gustaría trabajar en "+ myFuturePosition
 
-console.log("laater");
+console.log(frase);
