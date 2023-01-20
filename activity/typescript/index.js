af26add3b614248347f96fdb91e56c59c7aa8af9
FranJimenezQ <flakundo@gmail.com>
2023-01-20 08:20:00 -0600

    Cambios finales para actividad 3 typescript

--- activity/typescript/index.js
+++ activity/typescript/index.js
@@ -1,4 +1,38 @@
-// Ejercicio 1
+var getPerson = function (person) {
+    //return console.log(Object.values(person)) 
+};
+var person1 = { name: "Fran", age: 38, profession: "Developer" };
+getPerson(person1);
 // Ejercicio 2
-// Ejercicio 3
+function sumOrConcatenate(num1, num2) {
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        console.log(num1 + num2);
+    }
+    if (typeof num1 === "string" && typeof num2 === "string") {
+        console.log(num1 + "-" + num2);
+    }
+    if (typeof num1 === "number" && typeof num2 === "string" || typeof num1 === "string" && typeof num2 === "number") {
+        console.log(num1 + "+" + num2);
+    }
+}
+sumOrConcatenate(1, 2);
+sumOrConcatenate("Hola", "Mundo");
+sumOrConcatenate("Hola", 3);
+function OnVehicle(veh) {
+    return veh.turnOnEngine !== undefined;
+}
+function getvehicleInfo(veh) {
+    if (OnVehicle(veh)) {
+        veh.pressPedal;
+    }
+    else {
+        veh.openThrottle;
+    }
+}
 // Ejercicio 4
+var array1 = ['Hello', 'I', 'have', 3, 'tasks'];
+function StringNumArray(array) {
+    console.log(array.slice(1));
+}
+;
+StringNumArray(array1);
--- activity/typescript/index.ts
+++ activity/typescript/index.ts
@@ -1,7 +1,65 @@
 // Ejercicio 1
+interface Person {
+    name: string;
+    age: number;
+    profession: string;
+}
 
+let getPerson = (person: Person):void=> 
+{   
+     //return console.log(Object.values(person)) 
+   
+}
+
+let person1= {name: "Fran", age: 38, profession:"Developer"};
+getPerson(person1);
 // Ejercicio 2
+function sumOrConcatenate(num1: number | string, num2: number | string){
+    if(typeof num1 === "number" && typeof num2 === "number"){
+        console.log(num1+num2)
+
+    } if(typeof num1 === "string" && typeof num2 === "string"){
+        console.log(num1+"-"+num2)
+    }
+    if(typeof num1  === "number" && typeof num2 === "string" || typeof num1 === "string" && typeof num2 === "number" ){
+        console.log(num1+"+"+num2)
+    }
+}
+sumOrConcatenate(1,2)
+sumOrConcatenate("Hola","Mundo")
+sumOrConcatenate("Hola",3)
 
 // Ejercicio 3
+interface Car {
+    tires: number;
+    turnOnEngine:()=>void;
+    pressPedal:()=>void;
+}
+interface Motorcycle {
+    tires: number;
+    turnOnEngine:()=>void;
+    openThrottle:()=>void;
+}
+
+type Vehicle = Car | Motorcycle;
+function OnVehicle(veh: Vehicle): veh is Car{
+    return (veh as Car).turnOnEngine !== undefined;
+}
+
+function getvehicleInfo(veh: Vehicle) {
+    if(OnVehicle(veh)){
+        veh.pressPedal;
+    }else{
+        veh.openThrottle;
+    }
+}
 
 // Ejercicio 4
+const array1 = ['Hello', 'I', 'have', 3, 'tasks'];
+
+function StringNumArray(array: Array<string|number>){
+      console.log(array.slice(1))
+};
+
+
+StringNumArray(array1);
